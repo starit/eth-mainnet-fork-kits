4318aca44cee48a4ed3cf1161c3729140c95ba4a
starit <janyucheng@gmail.com>
2022-03-30 07:41:04 +0800

    fix cmd

--- send.sh
+++ send.sh
@@ -1,11 +1,21 @@
-npx hardhat run scripts/faucet_crvRenWBTC.js --network localhost
-npx hardhat run scripts/faucet_eCRV.js --network localhost
+#!/bin/bash
+# npx hardhat run scripts/faucet_crvRenWBTC.js --network localhost
+# npx hardhat run scripts/faucet_eCRV.js --network localhost
 # npx hardhat run scripts/faucet_hCRV.js --network localhost
-npx hardhat run scripts/faucet_threeCRV.js --network localhost
+# npx hardhat run scripts/faucet_threeCRV.js --network localhost
 #
 
 # npx hardhat run scripts/faucet_slpETHUSDC.js --network localhost
-npx hardhat run scripts/faucet_slpETHWBTC.js --network localhost
+# npx hardhat run scripts/faucet_slpETHWBTC.js --network localhost
 # npx hardhat run scripts/faucet_slpETHDai.js  --network localhost
 # npx hardhat run scripts/faucet_slpETHUSDT.js --network localhost
 
+symbols=('usdc' 'dai' 'usdt' 'weth' 'wbtc' 'slpETHWBTC')
+
+echo 'symbols:'${symbols[*]}
+echo '----'
+for symbol in "${symbols[@]}"
+do
+  echo 'symbol:'${symbol}
+  npx hardhat run scripts/faucet_${symbol}.js --network hardhat
+done
--- send_eth.sh
+++ send_eth.sh
@@ -1,2 +1,2 @@
-npx hardhat run scripts/faucet_eth.js --network localhost
+npx hardhat run scripts/faucet_eth.js --network hardhat
 
